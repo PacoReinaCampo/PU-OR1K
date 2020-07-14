d4b81c6cbd748839845401871c593fad6cf31edf
PacoReinaCampo <frareicam@gmail.com>
2020-07-14 21:54:15 +0200

    new commit

--- bench/cpp/jtagServer.h
+++ bench/cpp/jtagServer.h
@@ -37,7 +37,7 @@
  *
  * =============================================================================
  * Author(s):
- *   Francisco Javier Reina Campo <frareicam@gmail.com>
+ *   Paco Reina Campo <pacoreinacampo@queenfield.tech>
  */
 
 #ifndef __JTAG_SERVER_H__
--- bench/cpp/verilator_tb_utils.h
+++ bench/cpp/verilator_tb_utils.h
@@ -37,7 +37,7 @@
  *
  * =============================================================================
  * Author(s):
- *   Francisco Javier Reina Campo <frareicam@gmail.com>
+ *   Paco Reina Campo <pacoreinacampo@queenfield.tech>
  */
 
 #ifndef __VERILATOR_TB_UTILS_H__
